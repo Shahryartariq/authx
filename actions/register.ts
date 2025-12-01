cb076ac2715e4be00b7931e2a2acdf124b86b127
Muhammad Shehryar <Shahryartariq57@gmail.com>
2025-12-01 14:39:18 +0500

    Added Create Register User

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,11 @@
 "use server";
 
+import { db } from "@/lib/db";
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { get } from "http";
+import { getUserByEmail } from "@/data/user";
 
 async function register(values: z.infer<typeof RegisterSchema>) {
   console.log("Logging in with values:", values);
@@ -11,7 +15,29 @@ async function register(values: z.infer<typeof RegisterSchema>) {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email Sent!" };
+  const {email, password, name} =  validateFields.data; 
+  const hashedPassword = await bcrypt.hash(password, 10);
+  // console.log("Hashed password:", hashedPassword);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    console.log("User already exists with email:", email);
+    return { error: "Email already exists!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: SEND VERFICATION EMAIL
+
+ 
+  return { success: "User Created!" };
 }
 
 export { register };
